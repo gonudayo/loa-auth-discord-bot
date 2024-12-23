c0892bdae7951cef205c4ed7c6a7e6337528f49f
gonudayo <kgw010119@gmail.com>
2024-12-23 23:31:12 +0900

    two modals

--- index.js
+++ index.js
@@ -1,67 +1,126 @@
-const fs = require("node:fs");
-const path = require("node:path");
 const {
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle,
   Client,
-  Collection,
   Events,
   GatewayIntentBits,
-  MessageFlags,
+  InteractionType,
+  ModalBuilder,
+  TextInputBuilder,
+  TextInputStyle,
 } = require("discord.js");
-const { token } = require("./config.json");
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+// use dotenv
+const dotenv = require("dotenv");
+dotenv.config();
 
-client.commands = new Collection();
-const foldersPath = path.join(__dirname, "commands");
-const commandFolders = fs.readdirSync(foldersPath);
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
+});
 
-for (const folder of commandFolders) {
-  const commandsPath = path.join(foldersPath, folder);
-  const commandFiles = fs
-    .readdirSync(commandsPath)
-    .filter((file) => file.endsWith(".js"));
-  for (const file of commandFiles) {
-    const filePath = path.join(commandsPath, file);
-    const command = require(filePath);
-    if ("data" in command && "execute" in command) {
-      client.commands.set(command.data.name, command);
-    } else {
-      console.log(
-        `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
-      );
+client.on(Events.MessageCreate, (message) => {
+  if (message.author.bot) return;
+
+  let button = new ActionRowBuilder();
+  button.addComponents(
+    new ButtonBuilder()
+      .setCustomId("verification-button")
+      .setStyle(ButtonStyle.Primary)
+      .setLabel("인증 시작하기")
+  );
+  message.reply({
+    components: [button],
+  });
+});
+
+client.on(Events.InteractionCreate, async (interaction) => {
+  if (interaction.isButton()) {
+    if (interaction.customId === "verification-button") {
+      // 첫 번째 모달 표시
+      const modal = new ModalBuilder()
+        .setCustomId("nickname-modal")
+        .setTitle("닉네임 입력하기")
+        .addComponents([
+          new ActionRowBuilder().addComponents(
+            new TextInputBuilder()
+              .setCustomId("nickname-input")
+              .setLabel("닉네임")
+              .setStyle(TextInputStyle.Short)
+              .setMinLength(2)
+              .setMaxLength(20)
+              .setPlaceholder("이곳에 닉네임을 입력해 주세요.")
+              .setRequired(true)
+          ),
+        ]);
+
+      await interaction.showModal(modal);
     }
   }
-}
+
+  if (interaction.type === InteractionType.ModalSubmit) {
+    if (interaction.customId === "nickname-modal") {
+      const response = interaction.fields.getTextInputValue("nickname-input");
+
+      // 모달 제출 응답
+      await interaction.reply({
+        content: `제출한 닉네임: "${response}"`,
+        ephemeral: true, // 사용자에게만 보이도록 설정
+      });
+
+      console.log(response);
+
+      // 두 번째 버튼 제공
+      let secondButton = new ActionRowBuilder();
+      secondButton.addComponents(
+        new ButtonBuilder()
+          .setCustomId("profile-selection-button")
+          .setStyle(ButtonStyle.Primary)
+          .setLabel("인증하기")
+      );
+
+      // 버튼을 사용자에게 응답으로 제공
+      await interaction.followUp({
+        components: [secondButton],
+        ephemeral: true,
+      });
+    } else if (interaction.customId === "profile-modal") {
+      const profileResponse =
+        interaction.fields.getTextInputValue("profile-input");
+      await interaction.reply({
+        content: `제출되었습니다.`,
+        ephemeral: true,
+      });
+      console.log(profileResponse);
+    }
+  }
+
+  if (interaction.isButton()) {
+    if (interaction.customId === "profile-selection-button") {
+      // 두 번째 모달 표시
+      const secondModal = new ModalBuilder()
+        .setCustomId("profile-modal")
+        .setTitle("스토브 라운지 프로필 인증하기")
+        .addComponents([
+          new ActionRowBuilder().addComponents(
+            new TextInputBuilder()
+              .setCustomId("profile-input")
+              .setLabel("프로필 주소")
+              .setStyle(TextInputStyle.Short)
+              .setMinLength(3)
+              .setMaxLength(20)
+              .setPlaceholder("이곳에 프로필 주소를 입력해 주세요.")
+              .setRequired(true)
+          ),
+        ]);
+
+      await interaction.showModal(secondModal);
+    }
+  }
+});
 
 client.once(Events.ClientReady, (readyClient) => {
   console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
 
-client.on(Events.InteractionCreate, async (interaction) => {
-  if (!interaction.isChatInputCommand()) return;
-  const command = interaction.client.commands.get(interaction.commandName);
-
-  if (!command) {
-    console.error(`No command matching ${interaction.commandName} was found.`);
-    return;
-  }
-
-  try {
-    await command.execute(interaction);
-  } catch (error) {
-    console.error(error);
-    if (interaction.replied || interaction.deferred) {
-      await interaction.followUp({
-        content: "There was an error while executing this command!",
-        flags: MessageFlags.Ephemeral,
-      });
-    } else {
-      await interaction.reply({
-        content: "There was an error while executing this command!",
-        flags: MessageFlags.Ephemeral,
-      });
-    }
-  }
-});
-
-client.login(token);
+client.login(process.env.DISCORD_TOKEN);
